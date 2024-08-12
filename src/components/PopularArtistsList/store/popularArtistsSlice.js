a3d30652bebc0ef5ae57184208973b41f3350f42
Aslan Kh <lampi15rus@gmail.com>
2024-08-12 22:10:23 +0300

    added some css changes

--- src/components/PopularArtistsList/store/popularArtistsSlice.js
+++ src/components/PopularArtistsList/store/popularArtistsSlice.js
@@ -14,7 +14,7 @@ export const fetchPopularArtists = createAsyncThunk(
             const { data: artists } = await supabase
                 .from('artists')
                 .select()
-                .range(0, every ? 49 : 5);
+                .range(0, every ? 49 : 6);
             return artists;
         } catch (err) {
             return thunkApi.rejectWithValue(err);
