9fe00ceb3656e383ce480e815f6f2ba2f82abae7
Aslan Kh <lampi15rus@gmail.com>
2024-12-12 23:50:23 +0300

    Improve responsive styles and add media queries for the Sidebar

--- src/components/Sidebar/utils/handleAddSidebarInfo.js
+++ src/components/Sidebar/utils/handleAddSidebarInfo.js
@@ -13,7 +13,10 @@ const handleAddSidebarInfo = (
     sidebarType,
     sidebarList = null
 ) => {
-    if (classNames.every((className) => e.target.className !== className)) {
+    if (
+        classNames.every((className) => e.target.className !== className) &&
+        window.innerWidth > 600
+    ) {
         dispatch(setAddSidebarInfo(data));
         dispatch(setAddSidebarInfoType(sidebarType));
         dispatch(setToggleShowSidebar(true));
